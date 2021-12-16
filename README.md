1ef20650754db9bd2fe82cfe0d49868d8e8c772e
danielfranco16 <oncontato@hotmail.com>
2021-12-15 21:47:06 -0300

    pomodore timer em flutter  versão 1

---
+++ README.md
@@ -0,0 +1,3 @@
+A Flutter Pomodoro Timer.
+
+![Pomodoro Timer](https://github.com/natanportilho/Flutter-Pomodoro-Timer/blob/master/assets/pomodoro_timer.png)
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'pomodoro/pomodoro_timer_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pomodoro Timer',
+      home: PomodoroTimer(),
+    );
+  }
+}
